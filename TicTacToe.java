97df4658d4eab7a419e32cbd0f8fb572c87b51c9
im-Pushpendra <112095599+im-Pushpendra@users.noreply.github.com>
2022-10-20 21:20:06 +0530

    Added: creating-tictactoe-board-logic

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,9 +1,25 @@
 package com.bridgelabz.day7.UC_TicTacToe;
 
+import java.util.*;
 public class TicTacToe {
+    int i;
+    static char[] board = new char[10];
+    static char player, computer;
+    Scanner scan = new Scanner(System.in);
+
+    public void initialize() {
+        for (i = 1; i < 10; i++) {
+            board[i] = ' ';
+        }
+    }
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to TIC_TAC_TOE game");
+		 System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+	        System.out.println("---------");
+	        System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+	        System.out.println("---------");
+	        System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
 
 	}
 
